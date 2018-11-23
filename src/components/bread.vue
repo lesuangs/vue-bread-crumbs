7be22b89f9cbad53f10a80a453d458afb0b457f6
yoki@gmail.com <yoki@gmail.com>
2018-11-23 17:02:46 +0800

    初始化代码

---
+++ src/components/bread.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bread-crumb-box" v-text="titleText"> </div>
+</template>
+
+<script>
+	export default {
+	  props:{
+	    titleText:String,
+    },
+		data() {
+			return {}
+		},
+		computed: {},
+		watch: {},
+		methods: {},
+		created() {
+		},
+		mounted() {
+		},
+	}
+</script>
+
+<style lang='less' scoped>
+
+</style>
---
+++ src/components/demo1.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		computed: {},
+		watch: {},
+		methods: {},
+		created() {
+		},
+		mounted() {
+		},
+	}
+</script>
+
+<style lang='less' scoped>
+
+</style>
